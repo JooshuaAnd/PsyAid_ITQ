871e0c9062a2b33246871243f1b42117a922ad38
JooshuaAnd <jojo65058@gmail.com>
2026-07-27 16:37:15 +0700

    trial

--- app/Controllers/LandingController.php
+++ app/Controllers/LandingController.php
@@ -4,19 +4,24 @@ namespace App\Controllers;
 
 class LandingController extends BaseController
 {
-    // public function index(): string
-    // {
-    //     $data = [
-    //         'title' => 'PsyAid - Disaster Mental Health Command Center',
-    //         'isLoggedIn' => session()->get('logged_in') ?? false,
-    //         'role' => session()->get('role') ?? null,
-    //         'poskoId' => session()->get('posko_id') ?? null,
-    //     ];
+    public function index(): string
+    {
+        $data = [
+            'title' => 'PsyAid - Disaster Mental Health Command Center',
+            'isLoggedIn' => session()->get('logged_in') ?? false,
+            'role' => session()->get('role') ?? null,
+            'poskoId' => session()->get('posko_id') ?? null,
+        ];
 
-    //     return view('landing/index', $data);
-    // }
-    public function index()
-{
-    die('PsyAid OK');
-}
+        return view('landing/index', $data);
+    }
+//    public function index()
+//     {
+//         helper('url');
+
+//         return view('landing/index', [
+//             'title' => 'PsyAid Test'
+//         ]);
+//     }
+
 }
